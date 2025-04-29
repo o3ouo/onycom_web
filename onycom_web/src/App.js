e4dba5916b47ac8da7079fb24a2136687d2233d4
o3ouo <o3ouo@daum.net>
2025-04-29 14:00:08 +0900

    first commit

---
+++ onycom_web/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Main from "./page/Main";
+import Header from "./page/Header";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <Header />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
